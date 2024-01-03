0fa003858df45512206b99692ebb46c3a5eb6cdd
justinDev91 <justin@sustainsoft.eu>
2024-01-03 13:49:43 +0100

    feat(entreprise): add email UniqueEntity field to avoid duplicated client

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -8,8 +8,10 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: ClientRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already a client with this email')]
 class Client
 {
     #[ORM\Id]
